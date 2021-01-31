1829d30ae5f6d3f010edb78fdf45ce8fefd27ba2
Katya Zhukova <EkaterinaZhukova95@yandex.ru>
2021-01-31 12:50:29 +0400

    baseurl

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://auth.nomoreparties.co'; 
+export const BASE_URL = 'https://api.zhuekaterina.students.nomoredomains.rocks'; 
 
 export const register = (email, password) => {
     return fetch(`${BASE_URL}/signup`, {
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,7 @@
 export const apiInfo = {
-    baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-16',
+    baseUrl: 'https://api.zhuekaterina.students.nomoredomains.rocks',
     headers: {
-      authorization: '9e2464ed-9bf4-456d-960e-bc935e02e4f4',
+      'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
       'Content-Type': 'application/json'
     },
 };
